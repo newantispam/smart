55e20715e2ff029698703b79fd26384603fa301c
newantispam <newantispam@mihanmail.ir>
2016-06-21 12:56:05 +0000

    Update sudo.lua

--- plugins/sudo.lua
+++ plugins/sudo.lua
@@ -1,6 +1,6 @@
 do
 local function run(msg, matches)
-local sudo = 230477839
+local sudo = 166111645
     if matches[1]:lower() == "insudo" then
        channel_invite_user("chat#id"..msg.to.id, 'user#id'..sudo, ok_cb, false)
     end
