b9741be8e8ebc9a24c146d2cfd1d392f2d866782
newantispam <newantispam@mihanmail.ir>
2016-07-03 19:18:38 +0000

    Update salams.lua

--- plugins/salams.lua
+++ plugins/salams.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'ڛـلام بابايےツ'
+local text = 'ڛـلام بابا مهدی جونمツ'
 if matches[1] == 'سلام پسرم' or 'سلام' then
     if is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
